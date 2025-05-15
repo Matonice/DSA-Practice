ce1b59ad97825e3a8eec65148f19cfd8be3cdfe9
Matonice <omotoso.abdulmatin4@gmail.com>
2025-05-15 20:46:54 +0100

    solution for contains_duplicate problem

---
+++ Array_Hashing/contains_duplicate.py
@@ -0,0 +1,24 @@
+from typing import List
+
+""" Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+"""
+
+def hasDuplicate(nums: List[int]) -> bool:
+    nums.sort()
+    
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1]:
+            return True
+    return False
