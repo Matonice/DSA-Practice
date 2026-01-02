07ce599d2b2be9ffc5f27534f0a1a203cad7e119
Matonice <omotoso.abdulmatin4@gmail.com>
2026-01-02 21:25:56 +0100

    solution to replace elements with the greates element on the rightside

---
+++ Array_Hashing/replace_elements_with_greatest_elements_on_rightside.py
@@ -0,0 +1,31 @@
+"""You are given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Example 1:
+
+Input: arr = [2,4,5,3,1,2]
+
+Output: [5,5,3,2,2,-1]
+Example 2:
+
+Input: arr = [3,3]
+
+Output: [3,-1]
+Constraints:
+
+1 <= arr.length <= 10,000
+1 <= arr[i] <= 100,000"""
+
+from typing import List
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        right_max = -1
+
+        for i in range(len(arr) -1, -1, -1):
+            new_max = max(right_max, arr[i])
+            arr[i] = right_max
+            right_max = new_max
+
+        return arr
