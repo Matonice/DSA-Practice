fa420889c2cb0dd62d081c63c5dd0fe24237316a
Matonice <omotoso.abdulmatin4@gmail.com>
2026-01-07 23:19:03 +0100

    solution to the reverse string problem

---
+++ Two_Pointers/reverse_string.py
@@ -0,0 +1,28 @@
+"""You are given an array of characters which represents a string s. Write a function which reverses a string.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["n","e","e","t"]
+
+Output: ["t","e","e","n"]
+Example 2:
+
+Input: s = ["r","a","c","e","c","a","r"]
+
+Output: ["r","a","c","e","c","a","r"]"""
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) -1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
